b2491464e980c15a116461bcf724e46c15b79f0d
togetoge <togeika1@gmail.com>
2018-08-09 23:28:15 +0900

    sf2dからcitro2dに移行

--- source/header.h
+++ source/header.h
@@ -1,20 +1,5 @@
-#pragma once
-#ifndef _HEADER
-#define _HEADER
-
-#include <3ds.h>
-#include <sf2d.h>
-#include <sfil.h>
-#include <sftd.h>
-#include <sys/stat.h>
-#include <stdio.h>
 #include <string.h>
-#include <cstdlib>
-#include <cstring>
-#include <cstdio>
-#include <math.h>
-#include <time.h>
-#include <tremor/ivorbiscodec.h>
-#include <tremor/ivorbisfile.h>
-
-#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <3ds.h>
+#include <citro2d.h>
