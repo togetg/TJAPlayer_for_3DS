fbec3c724311792862a9584af348d23b89dc497b
togetoge <togeika1@gmail.com>
2018-08-19 01:22:39 +0900

    引数の変更

--- source/tja.h
+++ source/tja.h
@@ -4,4 +4,4 @@
 void tja_head_load(double *,double *,int *);
 void tja_notes_load();
 void tja_draw(int);
-void tja_to_notes(bool isDnon, bool isKa, int count, C2D_SpriteSheet spriteSheet);
+void tja_to_notes(bool isDnon, bool isKa, int count, C2D_Sprite sprites[12]);
