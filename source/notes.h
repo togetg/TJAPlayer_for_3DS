c318f0d01ae752891d67e81c20e5925f57a508e4
togetoge <togeika1@gmail.com>
2018-08-20 19:33:26 +0900

    コマンド,スプライト構造体の実装

--- source/notes.h
+++ source/notes.h
@@ -4,7 +4,7 @@
 void notes_main(
 	bool isDnon,
 	bool isKa, 
-	char tja_notes[2048][128],
+	char tja_notes[2048][Max_Notes_Section],
 	int cnt, 
 	char *tja_title, 
 	char *tja_subtitle, 
@@ -26,3 +26,56 @@ void notes_main(
 	char *tja_scoremode, 
 	C2D_Sprite sprites[12]
 );
+
+void notes_init();
+
+enum Notes_Kind {
+	Rest,		//休符
+	Don,		//ドン
+	Ka,			//カツ
+	BigDon,		//ドン(大)
+	BigKa,		//カツ(大)
+	Renda,		//連打開始
+	BigRenda,	//連打(大)開始
+	Balloon,	//風船開始
+	RendaEnd,	//連打終了
+	Potato,		//お芋音符開始
+};
+
+enum Sprite_Notes_Kind {	//スプライト用
+	dOn = 2,
+	kA,
+	bIg_don,
+	bIg_ka,
+	rEnda,
+	rEnda_fini,
+	bIg_renda,
+	bIg_renda_fini,
+	bAlloon,
+	jUdge_ryou,
+	jUdge_ka,
+	jUdge_fuka,
+};
+
+enum Command_Kind {
+	Start = 1,
+	End,
+	Bpmchange,
+	Gogostart,
+	Gogoend,
+	Measure,
+	Scroll,
+	Delay,
+	Section,
+	Branchstart,
+	Branchend,
+	N,
+	E,
+	M,
+	Levelhold,
+	Bmscroll,
+	Hbscroll,
+	Barlineoff,
+	Barlineon,
+
+};
