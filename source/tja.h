99a976db9734cc03a19c7734eb5acbc116a65f11
togetoge <togeika1@gmail.com>
2018-08-20 19:35:25 +0900

    関数の追加

--- source/tja.h
+++ source/tja.h
@@ -1,7 +1,14 @@
 #pragma once
 #include <citro2d.h>
 
+typedef struct {
+	int knd, data[3],test;
+	double val;
+	char* command,*notes,*value;
+} COMMAND_T;
+
 void tja_head_load(double *,double *,int *);
 void tja_notes_load();
 void tja_draw(int);
 void tja_to_notes(bool isDnon, bool isKa, int count, C2D_Sprite sprites[12]);
+void get_command_value(char* buf, COMMAND_T *Command);
