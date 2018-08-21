0d4e76268cc8ff169ee0d6da30e9db4b9a31e56b
togetoge <togeika1@gmail.com>
2018-08-21 18:51:18 +0900

    定数名の変更

--- source/header.h
+++ source/header.h
@@ -3,5 +3,5 @@
 #include <stdlib.h>
 #include <3ds.h>
 #include <citro2d.h>
-#define File_Name "syaruru"
-#define Max_Notes_Section 130	//一小節の最大ノーツ数+1
+#define File_Name "yugen"
+#define Max_Notes_Measure 130	//一小節の最大ノーツ数+1
