6b75eb6013a894decd6e1ab6fa9e30e648141f44
togetoge <togeika1@gmail.com>
2018-08-23 16:49:14 +0900

    定数の追加

--- source/header.h
+++ source/header.h
@@ -3,5 +3,11 @@
 #include <stdlib.h>
 #include <3ds.h>
 #include <citro2d.h>
+
 #define File_Name "yugen"
+
 #define Max_Notes_Measure 130	//ˆê¬ß‚ÌÅ‘åƒm[ƒc”+1
+#define Measure_Max 2048
+#define Notes_Area 400.0
+#define Notes_Judge 93
+#define Notes_Judge_Range 307
