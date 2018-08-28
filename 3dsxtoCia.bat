31ca564c47329d901f57c87ee79d48bbe56dedb3
Togetoge <togeika1@yahoo.co.jp>
2018-08-28 21:12:44 +0900

    ファイル名を変更

--- 3dsxtoCia.bat
+++ 3dsxtoCia.bat
@@ -1,3 +1,3 @@
-cxitool Tjaplayer_on_3DS.3dsx Tjaplayer_on_3DS.cxi --banner resources/banner.bin
-makerom -f cia -o Tjaplayer_on_3DS.cia    -target t -i Tjaplayer_on_3DS.cxi:0:0
-rm Tjaplayer_on_3DS.cxi
+cxitool TJAPlayer_on_3DS.3dsx TJAPlayer_on_3DS.cxi --banner resources/banner.bin
+makerom -f cia -o TJAPlayer_on_3DS.cia    -target t -i TJAPlayer_on_3DS.cxi:0:0
+rm TJAPlayer_on_3DS.cxi
