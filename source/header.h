c0d680982102d0302a38f7bf2a2fbe1eaf88f485
togetoge <togeika1@gmail.com>
2018-08-30 19:08:31 +0900

    判定枠の位置を調整,ファイルネームを"test"に変更

--- source/header.h
+++ source/header.h
@@ -5,10 +5,10 @@
 #include <citro2d.h>
 #include <limits.h>
 
-#define File_Name "sime"
+#define File_Name "test"
 
 #define Max_Notes_Measure 130	//ˆê¬ß‚ÌÅ‘åƒm[ƒc”+1
 #define Measure_Max 2048
 #define Notes_Area 400.0
-#define Notes_Judge 93
-#define Notes_Judge_Range 307
+#define Notes_Judge 95
+#define Notes_Judge_Range 305
