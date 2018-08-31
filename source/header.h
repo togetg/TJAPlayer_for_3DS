2155e43afc40f2fca9c0b3a325f00fbb266135a4
togetoge <togeika1@gmail.com>
2018-08-31 23:17:48 +0900

    画面の大きさの定数をこっちに移動

--- source/header.h
+++ source/header.h
@@ -5,10 +5,15 @@
 #include <citro2d.h>
 #include <limits.h>
 
-#define File_Name "test"
+#define File_Name "senbonzakura"
 
 #define Max_Notes_Measure 130	//ˆê¬ß‚ÌÅ‘åƒm[ƒc”+1
 #define Measure_Max 2048
 #define Notes_Area 400.0
 #define Notes_Judge 95
 #define Notes_Judge_Range 305
+
+#define TOP_WIDTH  400
+#define TOP_HEIGHT 240
+#define BOTTOM_WIDTH  320
+#define BOTTOM_HEIGHT 240
