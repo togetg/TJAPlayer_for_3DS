9db50165b5e8d4dde2b928077e8880074958a10c
togetoge <togeika1@gmail.com>
2018-09-09 20:15:17 +0900

    ノーツの判定座標を修正、スプライトを追加

--- source/header.h
+++ source/header.h
@@ -10,16 +10,17 @@
 #define Max_Notes_Measure 130	//ˆê¬ß‚ÌÅ‘åƒm[ƒc”+1
 #define Measure_Max 2048
 #define Notes_Area 400.0
-#define Notes_Judge 95
-#define Notes_Judge_Range 305
+#define Notes_Judge 93
+#define Notes_Judge_Range 307
 
 #define TOP_WIDTH  400
 #define TOP_HEIGHT 240
 #define BOTTOM_WIDTH  320
 #define BOTTOM_HEIGHT 240
 
-#define Sprite_Number 15
+#define Sprite_Number 21
 
 #define Notes_Max 512
 #define BarLine_Max 64
 #define Renda_Max 64
+#define Balloon_Max 64
