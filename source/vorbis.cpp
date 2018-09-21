ee735775b74e09f3bb1f713543c9835cb5c20c5c
togetoge <togeika1@gmail.com>
2018-09-21 22:58:30 +0900

    バッファを増加

--- source/vorbis.cpp
+++ source/vorbis.cpp
@@ -9,7 +9,7 @@
 static OggVorbis_File	vorbisFile;
 static vorbis_info		*vi;
 static FILE				*f;
-static const size_t		buffSize = 8000;// 8 * 4096;
+static const size_t		buffSize = 8192;// 8 * 4096;
 
 void setVorbis(struct decoder_fn* decoder){
 	decoder->init = &initVorbis;
