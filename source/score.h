32d98bc69b706d56bd0fac918a888b4fe418bdae
togetoge <togeika1@gmail.com>
2018-09-24 03:35:22 +0900

    特良を追加

--- source/score.h
+++ source/score.h
@@ -1,14 +1,18 @@
 ﻿void score_init();
 void scoer_debug();
 void score_update(int knd);
+void send_gogotime(bool temp);
+int round_down(int arg);
 
 enum Judge_Knd {
 	RYOU = 0,
+	SPECIAL_RYOU,
 	KA,
+	SPECIAL_KA,
 	FUKA,
 	THROUGH,
 	ROLL,
 	BIG_ROLL,
-	BALOON,
+	BALLOON,
 	BALLOON_BREAK,
 };
