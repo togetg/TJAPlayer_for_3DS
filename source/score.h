73ff7f9caecaaf98bcb85d0ff1065f6f645bebfa
togetoge <togeika1@gmail.com>
2018-09-25 21:35:46 +0900

    関数の追加

--- source/score.h
+++ source/score.h
@@ -3,6 +3,8 @@ void scoer_debug();
 void score_update(int knd);
 void send_gogotime(bool temp);
 int round_down(int arg);
+void score_init_after();
+void calc_base_score(MEASURE_T Measure[Measure_Max], char tja_notes[Measure_Max][Max_Notes_Measure]);
 
 enum Judge_Knd {
 	RYOU = 0,
