5b9c2ed94cd9692858815f430d442d59bde5dafc
togetoge <togeika1@gmail.com>
2018-09-26 23:12:15 +0900

    新配点の連打の得点を修正、ドンダフル配点に対応、分岐用の変数を実装、#SECTIONを実装

--- source/score.cpp
+++ source/score.cpp
@@ -5,17 +5,15 @@
 #include "score.h"
 
 bool isGOGO;
-int CurrentScore,CurrentRollCount,TotalRollCount,combo,init,diff,DiffMul,scoremode,HitScore,ScoreDiff,BaseCeilingPoint;
+int combo,init,diff,DiffMul,scoremode,HitScore,ScoreDiff,BaseCeilingPoint;
+int CurrentScore,TotalScore,CurrentRollCount, TotalRollCount,TotalRyouCount,TotalKaCount,TotalFukaCount, CurrentRyouCount, CurrentKaCount, CurrentFukaCount;
 double tmp;
 TJA_HEADER_T TJA_Header;
 char buf_score[160];
-
+double A, B, C, D;
 
 void score_init() {
 
-	CurrentScore = 0;
-	CurrentRollCount = 0;
-	TotalRollCount = 0;
 	combo = 0;
 	get_tja_header(&TJA_Header);
 	init = TJA_Header.scoreinit;
@@ -25,31 +23,35 @@ void score_init() {
 	HitScore = 0;
 	ScoreDiff = 0;
 	BaseCeilingPoint = 0;
-}
-
-void score_init_after() {
-
 	CurrentScore = 0;
+	TotalScore = 0;
 	CurrentRollCount = 0;
 	TotalRollCount = 0;
-	combo = 0;
-	DiffMul = 0;
-	HitScore = 0;
-	ScoreDiff = 0;
+	TotalRyouCount = 0;
+	TotalKaCount = 0;
+	TotalFukaCount = 0;
+	CurrentRyouCount = 0;
+	CurrentKaCount = 0;
+	CurrentFukaCount = 0;
 }
+
 
 void score_update(int knd) {
 
-	int PreScore = CurrentScore;
+	int PreScore = TotalScore;
 	bool isCombo = false;
 
-	if (scoremode == 1 || scoremode == 2) {
+	//if (scoremode == 1 || scoremode == 2) {
 
 		double GOGOMul;
 		if (isGOGO == true) GOGOMul = 1.2;
 		else GOGOMul = 1.0;
 
-		if (scoremode == 2) {	//新配点
+		if (scoremode == 0) {	//ドンダフル配点
+			if (combo < 200) DiffMul = 0;
+			else DiffMul = 1;
+		}
+		else if (scoremode == 2) {	//新配点
 			if (0 <= combo && combo < 9) DiffMul = 0;
 			else if (9 <= combo && combo < 29) DiffMul = 1;
 			else if (29 <= combo && combo < 49) DiffMul = 2;
@@ -66,79 +68,161 @@ void score_update(int knd) {
 		switch (knd) {
 
 		case RYOU:
+			TotalScore += round_down(HitScore*GOGOMul);
 			CurrentScore += round_down(HitScore*GOGOMul);
 			combo++;
 			isCombo = true;
+			TotalRyouCount++;
+			CurrentRyouCount++;
 			break;
 
 		case SPECIAL_RYOU:
+			TotalScore += round_down(HitScore * GOGOMul) * 2;
 			CurrentScore += round_down(HitScore * GOGOMul) * 2;
 			combo++;
 			isCombo = true;
+			TotalRyouCount++;
+			CurrentRyouCount++;
 			break;
 
 		case KA:
+			TotalScore += round_down(HitScore / 2);
 			CurrentScore += round_down(HitScore / 2);
 			combo++;
 			isCombo = true;
+			TotalKaCount++;
+			CurrentKaCount++;
 			break;
 
 		case SPECIAL_KA:
+			TotalScore += round_down(HitScore - 10);
 			CurrentScore += round_down(HitScore - 10);
 			combo++;
 			isCombo = true;
+			TotalKaCount++;
+			CurrentKaCount++;
 			break;
 
 		case FUKA:
-			CurrentScore += 0;
 			combo = 0;
+			TotalFukaCount++;
+			CurrentFukaCount++;
 			break;
 
 		case THROUGH:
 			combo = 0;
+			TotalFukaCount++;
+			CurrentFukaCount++;
 			break;
 
 		case BALLOON:
 		case ROLL:
-			if (isGOGO == true) CurrentScore += 360;
-			else CurrentScore += 300;
+			if (scoremode == 0 || scoremode == 1) {	//旧配点
+				if (isGOGO == true) {
+					TotalScore += 120;
+					CurrentScore += 120;
+				}
+				else {
+					TotalScore += 100;
+					CurrentScore += 100;
+				}
+			}
+			else if (scoremode == 2) {	//新配点
+				if (isGOGO == true) {
+					TotalScore += 360;
+					CurrentScore += 360;
+				}
+				else {
+					TotalScore += 300;
+					CurrentScore += 300;
+				}
+			}
+			CurrentRollCount++;
+			TotalRollCount++;
 			break;
 
 		case BIG_ROLL:
-			if (isGOGO == true) CurrentScore += 430;
-			else CurrentScore += 360;
+			if (scoremode == 0 || scoremode == 1) {	//旧配点
+				if (isGOGO == true) {
+					TotalScore += 430;
+					CurrentScore += 430;
+				}
+				else {
+					TotalScore += 360;
+					CurrentScore += 360;
+				}
+			}
+			else if (scoremode == 2) {	//新配点
+				if (isGOGO == true) {
+					TotalScore += 240;
+					CurrentScore += 240;
+				}
+				else {
+					TotalScore += 200;
+					CurrentScore += 200;
+				}
+			}
+			CurrentRollCount++;
+			TotalRollCount++;
 			break;
 
 		case BALLOON_BREAK:
-			if (isGOGO == true) CurrentScore += 6000;
-			else CurrentScore += 5000;
-			break;
+			if (isGOGO == true) {
+				TotalScore += 6000;
+				CurrentScore += 6000;
+			}
+			else {
+				TotalScore += 5000;
+				CurrentScore += 5000;
+			}
 			break;
 
 		default:
 			break;
 		}
-	}
+	//}
 
 	if (scoremode == 2) {	//100コンボ毎のボーナス(新配点のみ)
-		if (isCombo == true && combo % 100 == 0) CurrentScore += 10000;
+		if (isCombo == true && combo % 100 == 0) {
+			TotalScore += 10000;
+			CurrentScore += 10000;
+		}
 	}
-	ScoreDiff = CurrentScore - PreScore;
+	ScoreDiff = TotalScore - PreScore;
 }
 
 void scoer_debug() {
 
-	snprintf(buf_score, sizeof(buf_score), "%s %s    %d:%d    %s" ,TJA_Header.title, TJA_Header.subtitle,TJA_Header.course, TJA_Header.level,TJA_Header.wave);
+	snprintf(buf_score, sizeof(buf_score), "Scoremode:%d   Init:%d   Diff:%d", TJA_Header.scoremode, init, diff);
+	debug_draw(0, 10, buf_score);
+	snprintf(buf_score, sizeof(buf_score), "%s %s    Course:%d    Level:%d    %s" ,TJA_Header.title, TJA_Header.subtitle,TJA_Header.course, TJA_Header.level,TJA_Header.wave);
+	debug_draw(0, 30, buf_score);
+	snprintf(buf_score, sizeof(buf_score), "Score:%d    %dCombo    diff:%d",TotalScore, combo, ScoreDiff);
 	debug_draw(0, 150, buf_score);
-	snprintf(buf_score, sizeof(buf_score), "Scoremode:%d   Init:%d   Diff:%d",TJA_Header.scoremode,init, diff);
+	snprintf(buf_score, sizeof(buf_score), "Current   Score:%d    Roll:%d    Perfect:%.1f", CurrentScore, CurrentRollCount, (double)CurrentRyouCount/(CurrentRyouCount+CurrentKaCount+CurrentFukaCount)*100.0);
 	debug_draw(0, 160, buf_score);
-	snprintf(buf_score, sizeof(buf_score), "Score:%d    %dCombo    diff:%d",CurrentScore, combo, ScoreDiff);
-	debug_draw(0, 180, buf_score);
+	snprintf(buf_score, sizeof(buf_score), "%.0f:%.0f:%.0f",A,B,C);
+	debug_draw(0, 170, buf_score);
 	if (isGOGO == true) {
 		snprintf(buf_score, sizeof(buf_score), "GOGOTIME");
 		debug_draw(0, 190, buf_score);
 	}
-	debug_draw(100, 190, buf_score);
+}
+
+void branch_start(int knd,double x,double y) {
+
+	A = knd;
+	B = x;
+	C = y;
+}
+
+void branch_section_init() {	//#SECTION
+
+	CurrentRollCount = 0;
+	CurrentRyouCount = 0;
+	CurrentKaCount = 0;
+	CurrentFukaCount = 0;
+	CurrentScore = 0;
 }
 
 void send_gogotime(bool arg) {
@@ -265,12 +349,24 @@ void calc_base_score(MEASURE_T Measure[Measure_Max], char notes[Measure_Max][Max
 							RollCnt = (int)((roll_end_time - roll_start_time) / (1.0 / 12.0));
 
 							if (RollKnd == Roll) {
-								if (gogo == true) TmpBaseCeilingPoint -= RollCnt * 360;
-								else TmpBaseCeilingPoint -= RollCnt * 300;
+								if (scoremode == 1) {
+									if (gogo == true) TmpBaseCeilingPoint -= RollCnt * 360;
+									else TmpBaseCeilingPoint -= RollCnt * 300;
+								}
+								if (scoremode == 2) {
+									if (gogo == true) TmpBaseCeilingPoint -= RollCnt * 120;
+									else TmpBaseCeilingPoint -= RollCnt * 100;
+								}
 							}
 							else if (RollKnd == BigRoll) {
-								if (gogo == true) TmpBaseCeilingPoint -= RollCnt * 430 * gogo;
-								else TmpBaseCeilingPoint -= RollCnt * 360 * gogo;
+								if (scoremode == 1) {
+									if (gogo == true) TmpBaseCeilingPoint -= RollCnt * 430 * gogo;
+									else TmpBaseCeilingPoint -= RollCnt * 360 * gogo;
+								}
+								if (scoremode == 1) {
+									if (gogo == true) TmpBaseCeilingPoint -= RollCnt * 240 * gogo;
+									else TmpBaseCeilingPoint -= RollCnt * 200 * gogo;
+								}
 							}
 							roll_start_time = 0;
 							roll_end_time = 0;
@@ -283,6 +379,10 @@ void calc_base_score(MEASURE_T Measure[Measure_Max], char notes[Measure_Max][Max
 		}
 		diff = (TmpBaseCeilingPoint - (int)(combo / 100) * 10000) / (init_cnt * 4 + diff_cnt);
 		init = diff * 4;
+	}
+	else if (scoremode == 0) {
+		init = 1000;
+		diff = 1000;
 	}
 	//score_init_after();
 }
