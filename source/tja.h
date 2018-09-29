8b4de88a6cef300845fcaee7bcecf85cf4c3b604
togetoge <togeika1@gmail.com>
2018-09-30 00:48:01 +0900

    小節構造体にメンバ変数を追加

--- source/tja.h
+++ source/tja.h
@@ -5,7 +5,7 @@
 typedef struct {
 
 	int knd, data[3],test;
-	double val;
+	double val[3];
 	char* command_s,*notes,*value_s;
 } COMMAND_T;
 
@@ -19,11 +19,12 @@ typedef struct {
 }TJA_HEADER_T;
 
 typedef struct {
-	double judge_time, create_time,pop_time,
+	double judge_time, create_time, create_time_cmp, pop_time,
 		bpm, speed,measure,scroll;
 	int notes,firstmeasure,start_measure_count,max_notes,notes_count,command,
 		original_id;	//ソート前のid
 	bool flag,isDispBarLine;
+	
 } MEASURE_T;
 
 void tja_head_load();
