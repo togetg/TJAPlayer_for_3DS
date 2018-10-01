f2e50c220d8c47908637da4ba10d1c92d8f17baf
togetoge <togeika1@gmail.com>
2018-10-01 19:16:00 +0900

    関数の追加

--- source/score.h
+++ source/score.h
@@ -6,6 +6,7 @@ int round_down(int arg);
 void calc_base_score(MEASURE_T Measure[Measure_Max], char tja_notes[Measure_Max][Max_Notes_Measure]);
 void branch_section_init();
 int branch_start(int knd, double x, double y);
+void draw_lane(C2D_Sprite  sprites[Sprite_Number]);
 
 enum Judge_Knd {
 	RYOU = 0,
