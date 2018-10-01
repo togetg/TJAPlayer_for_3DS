5e4da55312cd5fce617b040beda202fb3a671358
togetoge <togeika1@gmail.com>
2018-10-01 19:13:30 +0900

    スプライトの数を増加

--- source/header.h
+++ source/header.h
@@ -5,10 +5,10 @@
 #include <citro2d.h>
 #include <limits.h>
 
-#define File_Name "そつおめしき"
+#define File_Name "さいたま2000"
 
 #define Max_Notes_Measure 130	//一小節の最大ノーツ数+1
-#define Measure_Max 2048
+#define Measure_Max 4096
 #define Notes_Area 400.0
 #define Notes_Judge 93
 #define Notes_Judge_Range 307
@@ -18,7 +18,7 @@
 #define BOTTOM_WIDTH  320
 #define BOTTOM_HEIGHT 240
 
-#define Sprite_Number 22
+#define Sprite_Number 28
 
 #define Notes_Max 512
 #define BarLine_Max 64
