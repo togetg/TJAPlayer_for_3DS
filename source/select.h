f4f675590c1904a798bec9c30d44545a4f9e2a49
togetoge <togeika1@gmail.com>
2018-10-08 23:27:37 +0900

    関数の追加

--- source/select.h
+++ source/select.h
@@ -3,10 +3,10 @@
 
 typedef struct {
 
-	int level[4],x,y;
-	char* tja, *ogg, *genre;
-	char title[256];
+	int level[4],x,y,tmp;
+	char title[256],path[256],tja[256],ogg[256],genre[256];
 } LIST_T;
 
-void load_file_list();
+void load_file_list_tjafiles();
 void disp_file_list();
+void load_file_main();
