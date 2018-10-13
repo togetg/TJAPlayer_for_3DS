726820e3f34ec289d2dd5ecc98445b18fab4d48a
togetoge <togeika1@gmail.com>
2018-10-13 13:13:32 +0900

    各コース難易度を表示するように

--- source/select.cpp
+++ source/select.cpp
@@ -11,12 +11,16 @@ void select_draw(float x, float y, const char *text);
 LIST_T List[List_Max];
 char buf_select[256];
 int SongNumber = 0;		//曲の総数
-int count = 0,cursor = 0;
+int count = 0,cursor = 0,course_cursor = 0,course_count = 0;
+bool isSelectCourse = false;
 
 void load_file_main() {
 
 	load_file_list_tjafiles();
 	cursor = 0;
+	course_cursor = 0;
+	course_count = 0;
+	isSelectCourse = false;
 }
 
 void load_file_list_tjafiles() {	//バグがおこるため再帰は使わない
@@ -52,33 +56,150 @@ void load_file_list_tjafiles() {	//バグがおこるため再帰は使わない
 
 
 void disp_file_list() {
-	
-	//select_draw(100, 0, "Press Select to start.");
+
+	int n = 0;
+	course_count = 0;
+
+	if (cursor > 0) cursor = -1*(SongNumber - 1);
+	if (cursor < -1 * (SongNumber - 1)) cursor = 0;
 
 	for (int i = 0; i < SongNumber; i++) {
 
-		select_draw(30, (i+cursor) * 20 + 60, List[i].title);
+		if ((n + cursor) * 20 + 60 >= 0 && (n + cursor) * 20 + 60 <= 220) {
 
-		snprintf(buf_select, sizeof(buf_select), "★x%d",List[i].level[ONI]);
-		select_draw(360, (i+cursor) * 20 + 60, buf_select );
+			select_draw(30, (n + cursor) * 20 + 60, List[i].title);
+
+			if (i != (cursor*-1)) {
+				snprintf(buf_select, sizeof(buf_select), "★x%d", List[i].level[ONI]);
+				select_draw(360, (n + cursor) * 20 + 60, buf_select);
+			}
+		}
+
+		n++;
+
+		if (i == (cursor*-1)) {
+
+			int level;
+
+			if (List[i].course[EDIT] == true) {
+
+				level = List[i].level[EDIT];
+				if (level > 10) level = 10;
+				for (int j = 0; j < level; j++) {
+					select_draw(200 + j * 10, (n + cursor) * 20 + 60, "★");
+				}
+				for (int j = 0; j < (10 - level); j++) {
+					select_draw(200 + (j + level) * 10, (n + cursor) * 20 + 60, "・");
+				}
+				select_draw(80, (n + cursor) * 20 + 60, "おに");
+				snprintf(buf_select, sizeof(buf_select), "★x%d", List[i].level[EDIT]);
+				select_draw(360, (n + cursor) * 20 + 60, buf_select);
+				n++;
+				course_count++;
+			}
+
+			if (List[i].course[ONI] == true) {
+
+				level = List[i].level[ONI];
+				if (level > 10) level = 10;
+				for (int j = 0; j < level; j++) {
+					select_draw(200 + j * 10, (n + cursor) * 20 + 60, "★");
+				}
+				for (int j = 0; j < (10 - level); j++) {
+					select_draw(200 + (j + level) * 10, (n + cursor) * 20 + 60, "・");
+				}
+				select_draw(80, (n + cursor) * 20 + 60, "おに");
+				snprintf(buf_select, sizeof(buf_select), "★x%d", List[i].level[ONI]);
+				select_draw(360, (n + cursor) * 20 + 60, buf_select);
+				n++;
+				course_count++;
+			}
+
+			if (List[i].course[HARD] == true) {
+
+				level = List[i].level[HARD];
+				if (level > 10) level = 10;
+				for (int j = 0; j < level; j++) {
+					select_draw(200 + j * 10, (n + cursor) * 20 + 60, "★");
+				}
+				for (int j = 0; j < (10 - level); j++) {
+					select_draw(200 + (j + level) * 10, (n + cursor) * 20 + 60, "・");
+				}
+				select_draw(80, (n + cursor) * 20 + 60, "むずかしい");
+				snprintf(buf_select, sizeof(buf_select), "★x%d", List[i].level[HARD]);
+				select_draw(360, (n + cursor) * 20 + 60, buf_select);
+				n++;
+				course_count++;
+			}
+
+			if (List[i].course[NORMAL] == true) {
+
+				level = List[i].level[NORMAL];
+				if (level > 10) level = 10;
+				for (int j = 0; j < level; j++) {
+					select_draw(200 + j * 10, (n + cursor) * 20 + 60, "★");
+				}
+				for (int j = 0; j < (10 - level); j++) {
+					select_draw(200 + (j + level) * 10, (n + cursor) * 20 + 60, "・");
+				}
+				select_draw(80, (n + cursor) * 20 + 60, "ふつう");
+				snprintf(buf_select, sizeof(buf_select), "★x%d", List[i].level[NORMAL]);
+				select_draw(360, (n + cursor) * 20 + 60, buf_select);
+				n++;
+				course_count++;
+			}
+
+			if (List[i].course[EASY] == true) {
+
+				level = List[i].level[EASY];
+				if (level > 10) level = 10;
+				for (int j = 0; j < level; j++) {
+					select_draw(200 + j * 10, (n + cursor) * 20 + 60, "★");
+				}
+				for (int j = 0; j < (10 - level); j++) {
+					select_draw(200 + (j + level) * 10, (n + cursor) * 20 + 60, "・");
+				}
+				select_draw(80, (n + cursor) * 20 + 60, "かんたん");
+				snprintf(buf_select, sizeof(buf_select), "★x%d", List[i].level[EASY]);
+				select_draw(360, (n + cursor) * 20 + 60, buf_select);
+				n++;
+				course_count++;
+			}
+
+			if (isSelectCourse == true) {
+
+				select_draw(60, (course_cursor+1) * 20 + 60, ">>");
+			}
+		}
 	}
 
-	select_draw(10, 60, "◆");
+	select_draw(10, 60, ">>");
+	//snprintf(buf_select, sizeof(buf_select), "%d:%d",cursor,SongNumber);
+	//select_draw(0, 60, buf_select);
 }
 
 void cursor_update(int knd) {
 
-	if (knd == 0) {			//上
-		cursor++;
+	if (knd == KEY_UP) {
+		if (isSelectCourse == false) cursor++;
+		else if (course_cursor > 0) course_cursor--;
 	}
-	else if (knd == 1) {	//下
-		cursor--;
+	else if (knd == (int)KEY_DOWN) {
+		if (isSelectCourse == false) cursor--;
+		else if (course_cursor < (course_count-1)) course_cursor++;
 	}
-	else if (knd == 2) {	//右
-		cursor -= 5;
+	else if (knd == KEY_RIGHT) {
+		if (isSelectCourse == false) cursor -= 5;
 	}
-	else if (knd == 3) {	//左
-	cursor += 5;
+	else if (knd == KEY_LEFT) {
+		if (isSelectCourse == false) cursor += 5;
+	}
+	else if (knd == KEY_A && course_count != 0) {
+		isSelectCourse = true;
+	}
+	else if (knd == KEY_B) {
+		isSelectCourse = false;
+		course_cursor = 0;
 	}
 }
 
