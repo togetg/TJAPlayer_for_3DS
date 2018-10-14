d05bbf4aba21db1f14cf97601fc0141167ecee3a
togetoge <togeika1@gmail.com>
2018-10-14 12:26:00 +0900

    関数の追加

--- source/select.h
+++ source/select.h
@@ -12,5 +12,6 @@ void load_file_list_tjafiles();
 void disp_file_list();
 void load_file_main();
 void cursor_update(int knd);
-void get_SelectedId(LIST_T *TMP);
-void select_draw(float x, float y, const char *text);
+void get_SelectedId(LIST_T *TMP, int *arg);
+void draw_select(float x, float y, const char *text);
+bool get_isGameStart();
