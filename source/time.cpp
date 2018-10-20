7c56dcbc67c3ae112d4f61ddd28c563ecc57afc3
togetoge <togeika1@gmail.com>
2018-10-21 00:19:32 +0900

    時間の計測を止められるように

--- source/time.cpp
+++ source/time.cpp
@@ -12,26 +12,57 @@ enum msec_status {
 
 struct timeval myTime;
 int cnt[5], msec[5][4], sec[5];
+int isStop[5];
+double PreTime[5],Time[5];
 
 double time_now(int id) {
 
-	gettimeofday(&myTime, NULL);
+	if (isStop[id] != 1) {
+		gettimeofday(&myTime, NULL);
 
-	if (cnt[id] == 0) msec[id][InIT] = (int)myTime.tv_usec;
+		if (cnt[id] == 0) msec[id][InIT] = (int)myTime.tv_usec;
 
-	msec[id][CuRRENT] = (int)myTime.tv_usec;
-	msec[id][LaST_DiFF] = msec[id][DiFF];
-	msec[id][DiFF] = msec[id][CuRRENT] - msec[id][InIT];
+		msec[id][CuRRENT] = (int)myTime.tv_usec;
+		msec[id][LaST_DiFF] = msec[id][DiFF];
+		msec[id][DiFF] = msec[id][CuRRENT] - msec[id][InIT];
 
-	if (msec[id][DiFF] < 0) msec[id][DiFF] = 1000000 - msec[id][DiFF] * -1;
+		if (msec[id][DiFF] < 0) msec[id][DiFF] = 1000000 - msec[id][DiFF] * -1;
 
-	if (msec[id][LaST_DiFF] > msec[id][DiFF]) sec[id]++;
+		if (msec[id][LaST_DiFF] > msec[id][DiFF]) sec[id]++;
 
+		cnt[id]++;
+		//printf("%04d:%06d\n", sec, msec[id][DiFF]);
+		Time[id] = sec[id] + msec[id][DiFF] / 1000000.0 + PreTime[id];
+	}
+	return Time[id];
+}
 
-	cnt[id]++;
-	//printf("%04d:%06d\n", sec, msec[id][DiFF]);
+void restart_time(int id) {
+	isStop[id] = 0;
+}
 
-	return sec[id]+msec[id][DiFF]/1000000.0;
+void stop_time(int id) {
+
+	isStop[id] = 1;
+	for (int n = 0; n < 4; n++) {
+		msec[id][n] = 0;
+	}
+
+	PreTime[id] = Time[id] += 0.0178;
+	sec[id] = 0;
+	cnt[id] = 0;
+}
+
+void toggle_time(int id) {
+
+	if (Time[id] != 0) {
+		if (isStop[id] == 1) restart_time(id);
+		else stop_time(id);
+	}
+}
+
+int get_time_isStop(int id) {
+	return isStop[id];
 }
 
 #define Fps_Sample 60
@@ -64,7 +95,9 @@ void time_ini() {
 
 		sec[i] = 0;
 		cnt[i] = 0;
-
+		isStop[i] = 0;
+		PreTime[i] = 0;
+		Time[i] = 0;
 	}
 	fps_time[0] = 0;
 	fps_time[1] = 0;
