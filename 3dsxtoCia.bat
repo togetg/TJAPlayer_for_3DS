a99cf8c940e3d12812239c6136f64dbea7890339
togetg <togeika1@gmail.com>
2018-10-23 17:27:29 +0900

    アプリタイトルを変更

--- 3dsxtoCia.bat
+++ 3dsxtoCia.bat
@@ -1,3 +1,4 @@
-cxitool TJAPlayer_on_3DS.3dsx TJAPlayer_on_3DS.cxi --banner resources/banner.bin
-makerom -f cia -o TJAPlayer_on_3DS.cia    -target t -i TJAPlayer_on_3DS.cxi:0:0
-rm TJAPlayer_on_3DS.cxi
+cxitool TJAPlayer_for_3DS.3dsx TJAPlayer_for_3DS.cxi --banner resources/banner.bin
+makerom -f cia -o TJAPlayer_for_3DS.cia -target t -i TJAPlayer_for_3DS.cxi:0:0
+rm TJAPlayer_for_3DS.cxi
+pause
