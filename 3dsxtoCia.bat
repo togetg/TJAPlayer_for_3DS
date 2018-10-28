e7e94595f8b8b000f98e2671bbe894fc191b6174
togetg <togeika1@gmail.com>
2018-10-28 21:09:41 +0900

    TitleIDを変更

--- 3dsxtoCia.bat
+++ 3dsxtoCia.bat
@@ -1,4 +1,4 @@
-cxitool TJAPlayer_for_3DS.3dsx TJAPlayer_for_3DS.cxi --banner resources/banner.bin
+cxitool TJAPlayer_for_3DS.3dsx TJAPlayer_for_3DS.cxi --banner resources/banner.bin --tid 0004000000B76765
 makerom -f cia -o TJAPlayer_for_3DS.cia -target t -i TJAPlayer_for_3DS.cxi:0:0
 rm TJAPlayer_for_3DS.cxi
 pause
