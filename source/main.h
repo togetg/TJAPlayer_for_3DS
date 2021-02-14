f90be76d7850fef9aba33b9fa973d469aa9eca85
togetg <togeika1@gmail.com>
2021-02-14 23:39:14 +0900

    関数,定数の追加

--- source/main.h
+++ source/main.h
@@ -3,3 +3,8 @@
 void draw_debug(float x, float y, const char *text);
 bool get_isPause();
 void debug_touch(int x, int y);
+bool get_isMusicStart();
+char* get_buffer();
+int powi(int x, int y);
+
+#define BUFFER_SIZE 160 //バッファ用文字列のサイズ
