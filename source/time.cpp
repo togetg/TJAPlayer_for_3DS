a01bf78997faa69a6b58ef8ea0ed1ea1c90dab1e
togetg <togeika1@gmail.com>
2021-02-14 23:40:52 +0900

    変数の追加

--- source/time.cpp
+++ source/time.cpp
@@ -1,7 +1,9 @@
 ﻿#include "header.h"
 #include "main.h"
+#include "vorbis.h"
+#include "tja.h"
+#include "time.h"
 #include <sys/time.h>
-char buf_time[160];
 
 enum msec_status {
 	InIT,
@@ -14,9 +16,18 @@ enum msec_status {
 struct timeval myTime;
 int cnt[TIME_NUM], msec[TIME_NUM][4], sec[TIME_NUM];
 int isStop[TIME_NUM];
-double PreTime[TIME_NUM],Time[TIME_NUM];
+double PreTime[TIME_NUM],Time[TIME_NUM],CurrentTime[TIME_NUM],IniVorbisTime[TIME_NUM];
 
 double get_current_time(int id) {
+	
+	if ((id == 0 || id == 1) && get_isMusicStart() == true) { //メインのカウントの時はVorbis基準の時間を返す 要曲終了時の処理
+		//if (isStop[id] != 1) stop_time(id);
+		if (CurrentTime[id] == 0 && Time[id] == 0 && IniVorbisTime[id]==0) IniVorbisTime[id] = getVorbisTime();
+		CurrentTime[id] = Time[id] + getVorbisTime() - IniVorbisTime[id];
+		//return CurrentTime[id]; 
+		snprintf(get_buffer(), BUFFER_SIZE, "vbt:%.1f", CurrentTime[id]);
+		//draw_debug(100, id*10, get_buffer()); 
+	}
 
 	if (isStop[id] != 1) {
 		gettimeofday(&myTime, NULL);
@@ -35,6 +46,8 @@ double get_current_time(int id) {
 		//printf("%04d:%06d\n", sec, msec[id][DiFF]);
 		Time[id] = sec[id] + msec[id][DiFF] / 1000000.0 + PreTime[id];
 	}
+	snprintf(get_buffer(), BUFFER_SIZE, "t:%.1f", Time[id]);
+	//draw_debug(0, id*10, get_buffer());
 	return Time[id];
 }
 
@@ -81,8 +94,8 @@ void draw_fps() {
 		fps = Fps_Sample / fps_sum;
 		fps_sum = 0;
 	}
-	snprintf(buf_time, sizeof(buf_time), "%.1ffps", fps);
-	draw_debug(300, 0, buf_time);
+	snprintf(get_buffer(), BUFFER_SIZE, "%.1ffps", fps);
+	draw_debug(300, 0, get_buffer());
 }
 
 void time_ini() {
@@ -99,6 +112,8 @@ void time_ini() {
 		isStop[i] = 0;
 		PreTime[i] = 0;
 		Time[i] = 0;
+		CurrentTime[i] = 0;
+		IniVorbisTime[i] = 0;
 	}
 	fps_time[0] = 0;
 	fps_time[1] = 0;
